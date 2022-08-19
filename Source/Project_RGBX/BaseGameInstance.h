3d8c0d00d487cf4bb378d02db76c7c99699706c7
Timothy Abiade <95978549+timo3abi@users.noreply.github.com>
2022-08-19 20:17:26 +0100

    Character Select Update
    Character Select Screen + Main Menu Level added

--- Source/Project_RGBX/BaseGameInstance.h
+++ Source/Project_RGBX/BaseGameInstance.h
@@ -9,9 +9,26 @@
 /**
  * 
  */
+
+UENUM(BlueprintType)
+enum class EFighterTag : uint8
+{
+	VE_Canvas UMETA(DisplayName = "Canvas"),
+	VE_Gris UMETA(DisplayName = "Gris"),
+	VE_Mauve UMETA(DisplayName = "Mauve"),
+	VE_Marigold UMETA(DisplayName = "Marigold"),
+	VE_Azul UMETA(DisplayName = "Azul"),
+	VE_Carmine UMETA(DisplayName = "Carmine"),
+	VE_Jade UMETA(DisplayName = "Jade"),
+	VE_Dummy UMETA(DisplayName = "Dummy")
+};
+
 UCLASS()
 class PROJECT_RGBX_API UBaseGameInstance : public UGameInstance
 {
 	GENERATED_BODY()
-	
+
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Player References")
+	EFighterTag fighterTag;
 };
--- Source/Project_RGBX/Project_RGBXGameMode.h
+++ Source/Project_RGBX/Project_RGBXGameMode.h
@@ -7,17 +7,6 @@
 #include "Project_RGBXCharacter.h"
 #include "Project_RGBXGameMode.generated.h"
 
-UENUM(BlueprintType)
-enum class EFighterTag : uint8
-{
-	VE_Dummy UMETA(DisplayName = "Dummy"),
-	VE_Shoto UMETA(DisplayName = "Shoto"),
-	VE_Charge UMETA(DisplayName = "Charge"),
-	VE_Rekka UMETA(DisplayName = "Rekka"),
-	VE_Zoner UMETA(DisplayName = "Zoner"),
-	VE_Rushdown UMETA(DisplayName = "Rushdown"),
-	VE_Grappler UMETA(DisplayName = "Grappler")
-};
 
 UCLASS(minimalapi)
 class AProject_RGBXGameMode : public AGameModeBase
